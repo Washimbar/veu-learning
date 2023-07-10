f1eb5745ab6056571f4da73e5970ce316f37a774
Washimbar <washimbari0001@gmail.com>
2023-07-10 20:46:47 +0530

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div>{{greet}} I am {{name}} . I am {{age}} years old.</div>
+    <div v-html="ex"></div>
+  </div>
+</template>
+
+<script>
+export default ({
+  name: 'app',
+  data() {
+    return{
+      greet: 'Hello',
+      name: 'Washim Bari',
+      age: 25,
+      ex: '<b>Hello</b>'
+    }
+  },
+})
+</script>
+
+<style>
+
+</style>
