1ff281dd81b38cdb8da357af7d27d4786b7e8ec7
Washimbar <washimbari0001@gmail.com>
2023-07-11 17:34:47 +0530

    vue class

--- src/App.vue
+++ src/App.vue
@@ -8,18 +8,28 @@
   <h1>HTML Attributes Binding:-</h1>
   <Ex03BindingAttributes />
   <hr />
+  <h1>HTML Classes Binding:-</h1>
+  <Ex04BindingClasses />
+  <hr />
+  <h1>HTML Inline style Binding:-</h1>
+  <Ex05BindingStyles />
+  <hr />
 </template>
 
 <script>
 import Ex01BindingText from "./components/01Binding/Ex01BindingText.vue";
 import Ex02BindingHTML from "./components/01Binding/Ex02BindingHTML.vue";
 import Ex03BindingAttributes from "./components/01Binding/Ex03BindingAttributes.vue";
+import Ex04BindingClasses from "./components/01Binding/Ex04BindingClasses.vue";
+import Ex05BindingStyles from "./components/01Binding/Ex05BindingStyles.vue";
 export default {
   name: "App",
   components: {
     Ex01BindingText: Ex01BindingText,
     Ex02BindingHTML: Ex02BindingHTML,
     Ex03BindingAttributes: Ex03BindingAttributes,
+    Ex04BindingClasses: Ex04BindingClasses,
+    Ex05BindingStyles: Ex05BindingStyles,
   },
 };
 </script>
---
+++ src/components/01Binding/Ex04BindingClasses.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <!-- Simply class add -->
+    <h2 class="underline">Heading 2 </h2>
+    <!-- Dynamically added class -->
+    <h4 class="underline" v-bind:class="status">Statues</h4>
+    <!-- Class added with condition -->
+    <h4  v-bind:class="isPromoted && 'promoted'">Statues</h4>
+    <!-- Class added with ternary condition -->
+    <h4  v-bind:class="isSoldOut ? 'booked' : 'new'">Ticket</h4>
+    <!-- Class bind using array -->
+    <h2 v-bind:class="['new','promoted']">Newly Promoted movie</h2>
+    <!-- Class added using array with ternary condition -->
+    <h2 v-bind:class="[isSoldOut ? 'booked' : 'new','promoted']">Newly Promoted movie</h2>
+    <!-- Class added using object -->
+    <h2 v-bind:class="{
+      promoted: isPromoted, 
+      booked: isSoldOut,
+      'sold-out': isSoldOut,
+    }">Review of Movie is 7.5</h2>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+        status: 'success',
+        isPromoted: true,
+        isSoldOut: true,
+    };
+  },
+};
+</script>
+
+<style>
+    .underline {
+        text-decoration: underline;
+    }
+    .promoted {
+      color: green;
+    }
+    .new {
+      color: powderblue;
+    }
+    .booked{
+      color: yellowgreen;
+    }
+</style>
